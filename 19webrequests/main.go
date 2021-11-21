3184dd0741ef0f9d1aa2ce7ea53d4471cdfed2bb
Sarvang Dave <sarvang00@gmail.com>
2021-11-21 12:30:46 +0530

    Handling web requests 101

---
+++ 19webrequests/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const url string = "https://lco.dev"
+
+func main() {
+	fmt.Println("LCO Web request")
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Response is of type: %T\n", response)
+
+	defer response.Body.Close() //Calller's responsibility to close
+
+	databytes, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	content := string(databytes)
+	fmt.Println(content)
+}
